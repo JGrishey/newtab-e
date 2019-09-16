4bcb39e13ad6036c531ebd3dcec8dbdd8b1a4275
JGrishey <jgrishey@gmail.com>
2019-09-15 23:33:46 -0400

    Updates

---
+++ precache-manifest.7a1b80d4f06ea8ccc4a7785c40ca9d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c046b57740e4ecebf7379dada28313f",
+    "url": "/newtab/index.html"
+  },
+  {
+    "revision": "c17cc66b6a96102fdb7b",
+    "url": "/newtab/static/css/main.8ebf3e91.chunk.css"
+  },
+  {
+    "revision": "15e1e8bdb793bf3eaca8",
+    "url": "/newtab/static/js/2.cf1ac5ef.chunk.js"
+  },
+  {
+    "revision": "c17cc66b6a96102fdb7b",
+    "url": "/newtab/static/js/main.cc1dcfbe.chunk.js"
+  },
+  {
+    "revision": "b5d42de9c0aa6a75d5cb",
+    "url": "/newtab/static/js/runtime~main.b3789fa8.js"
+  },
+  {
+    "revision": "d5f4cb92a376d742fb5c5537b1d5dba9",
+    "url": "/newtab/static/media/_DSC0646.d5f4cb92.jpg"
+  }
+]);
